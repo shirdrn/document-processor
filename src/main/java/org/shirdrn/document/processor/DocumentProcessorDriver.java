5d7fb94cc3db71388810cf224042bec08dee7244
shirdrn <shirdrn@gmail.com>
2013-12-01 23:08:33 +0800

    Update processor driver logic.

--- src/main/java/org/shirdrn/document/processor/DocumentProcessorDriver.java
+++ src/main/java/org/shirdrn/document/processor/DocumentProcessorDriver.java
@@ -14,26 +14,31 @@ public class DocumentProcessorDriver {
 
 	public static void main(String[] args) {
 		Context context = new Context();
+		boolean isTrainOpen = 
+				context.getConfiguration().getBoolean("processor.dataset.train.isopen", false);
+		Component[] chain = null;
+		if(isTrainOpen) {
+			// for train data
+			chain = new Component[] {
+					new BasicContextInitializer(context),
+					new CollectingTrainDocumentWords(context),
+					new DocumentTFIDFComputation(context),
+					new DenoisingDocumentTerms(context),
+					new OutputtingQuantizedTrainData(context)
+				};
+			prepareAndRun(chain);
+		} else {
+			// for test data
+			chain = new Component[] {
+					new BasicContextInitializer(context),
+					new CollectingTestDocumentWords(context),
+					new DocumentTFIDFComputation(context),
+					new DenoisingDocumentTerms(context),
+					new OutputtingQuantizedTestData(context)
+				};
+			prepareAndRun(chain);
+		}
 		
-		// for train data
-		Component[] chain = new Component[] {
-			new BasicContextInitializer(context),
-			new CollectingTrainDocumentWords(context),
-			new DocumentTFIDFComputation(context),
-			new DenoisingDocumentTerms(context),
-			new OutputtingQuantizedTrainData(context)
-		};
-		prepareAndRun(chain);
-		
-		// for test data
-//		Component[] chain = new Component[] {
-//				new BasicContextInitializer(context),
-//				new CollectingTestDocumentWords(context),
-//				new DocumentTFIDFComputation(context),
-//				new DenoisingDocumentTerms(context),
-//				new OutputtingQuantizedTestData(context)
-//			};
-//		prepareAndRun(chain);
 	}
 
 	private static void prepareAndRun(Component[] chain) {
