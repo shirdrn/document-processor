bb935d4e63d7727576e30d1c9b73fd8ff739e324
Yanjun <shiyanjun@yeah.net>
2013-12-04 15:32:33 +0800

    Update test component classes.

--- src/main/java/org/shirdrn/document/processor/component/test/OutputtingQuantizedTestData.java
+++ src/main/java/org/shirdrn/document/processor/component/test/OutputtingQuantizedTestData.java
@@ -1,8 +1,6 @@
 package org.shirdrn.document.processor.component.test;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -31,9 +29,8 @@ public class OutputtingQuantizedTestData extends AbstractOutputtingQuantizedData
 	@Override
 	protected void quantizeTermVectors() {
 		// load label vector
+		LOG.info("Load label vector...");
 		loadLabelVector();
-		// load term vectors
-		loadTermVectors();
 	}
 
 	private void loadLabelVector() {
@@ -43,7 +40,8 @@ public class OutputtingQuantizedTestData extends AbstractOutputtingQuantizedData
 		Map<Integer, String> globalIdToLabelMap = new HashMap<Integer, String>(0);
 		BufferedReader reader = null;
 		try {
-			reader = new BufferedReader(new InputStreamReader(new FileInputStream(labelVectorFile), charSet));
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(
+					context.getFDMetadata().getLabelVectorFile()), charSet));
 			String line = null;
 			while((line = reader.readLine()) != null) {
 				line = line.trim();
@@ -70,52 +68,9 @@ public class OutputtingQuantizedTestData extends AbstractOutputtingQuantizedData
 				}
 			}
 		}
+		LOG.info("Loaded: globalIdToLabelMap=" + globalIdToLabelMap);
 		context.getVectorMetadata().putIdToLabelPairs(globalIdToLabelMap);
 		context.getVectorMetadata().putLabelToIdPairs(globalLabelToIdMap);
-	}
-	
-	private void loadTermVectors() {
-		File[] vectorFiles = outputDir.listFiles(new FileFilter() {
-			@Override
-			public boolean accept(File pathname) {
-				return pathname.getAbsolutePath().endsWith("-terms.txt");
-			}
-		});	
-		for(File file : vectorFiles) {
-			loadTermVectorData(file);
-		}
-	}
-
-	private void loadTermVectorData(File file) {
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), charSet));
-			String line = null;
-			while((line = reader.readLine()) != null) {
-				line = line.trim();
-				if(!line.isEmpty()) {
-					String[] aLabel = line.split("\\s+");
-					if(aLabel.length == 3) {
-						int labelId = Integer.parseInt(aLabel[0]);
-						String word = aLabel[1];
-						int wordId = Integer.parseInt(aLabel[2]);
-						context.getVectorMetadata().putWordWithId(labelId, word, wordId);
-					}
-				}
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if(reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}		
 	}
 	
 }
