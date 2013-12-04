c2c18825da767121cb347d35df0c82be58e1c76f
Yanjun <shiyanjun@yeah.net>
2013-12-04 15:31:48 +0800

    Update training component classes.

--- src/main/java/org/shirdrn/document/processor/component/train/OutputtingQuantizedTrainData.java
+++ src/main/java/org/shirdrn/document/processor/component/train/OutputtingQuantizedTrainData.java
@@ -30,8 +30,8 @@ public class OutputtingQuantizedTrainData extends AbstractOutputtingQuantizedDat
 	@Override
 	public void fire() {
 		super.fire();
-		LOG.info("Output label mapping file: labelFile=" + labelVectorFile);
-		output(labelVectorFile, context.getVectorMetadata().labelVectorMapIterator());
+		LOG.info("Output label mapping file: labelFile=" + context.getFDMetadata().getLabelVectorFile());
+		output(context.getFDMetadata().getLabelVectorFile(), context.getVectorMetadata().labelVectorMapIterator());
 	}
 	
 	private void output(File file, Iterator<Entry<String, Integer>> iter) {
@@ -65,14 +65,9 @@ public class OutputtingQuantizedTrainData extends AbstractOutputtingQuantizedDat
 		Map<String, Integer> globalLabelToIdMap = new HashMap<String, Integer>(0);
 		// store all <labelId, label> pairs
 		Map<Integer, String> globalIdToLabelMap = new HashMap<Integer, String>(0);
-		// store all <word, wordId> pairs
-		Map<String, Integer> globalTermsMap = new HashMap<String, Integer>(0);
-		// iterate collected document words
-		Iterator<Entry<String, Map<String, Map<String, Term>>>> iter = context.getVectorMetadata().termTableIterator();
-		while(iter.hasNext()) {
-			Entry<String, Map<String, Map<String, Term>>> labelledDocsEntry = iter.next();
-			String label = labelledDocsEntry.getKey();
-			// generate label id
+		
+		// generate label id
+		for(String label : context.getVectorMetadata().getLabels()) {
 			Integer labelId = globalLabelToIdMap.get(label);
 			if(labelId == null) {
 				++labelNumber;
@@ -80,84 +75,56 @@ public class OutputtingQuantizedTrainData extends AbstractOutputtingQuantizedDat
 				globalLabelToIdMap.put(label, labelId);
 				globalIdToLabelMap.put(labelId, label);
 			}
-			Map<String, Map<String, Term>>  docs = labelledDocsEntry.getValue();
-			Iterator<Entry<String, Map<String, Term>>> docsIter = docs.entrySet().iterator();
-			while(docsIter.hasNext()) {
-				StringBuffer line = new StringBuffer();
-				line.append(labelId).append(" ");
-				Entry<String, Map<String, Term>> docsEntry = docsIter.next();
-				Map<String, Term> terms = docsEntry.getValue();
-				for(Entry<String, Term> termEntry : terms.entrySet()) {
-					String word = termEntry.getKey();
-					// generate word id
-					Integer wordId = globalTermsMap.get(word);
-					if(wordId == null) {
-						++wordNumber;
-						wordId = wordNumber;
-						globalTermsMap.put(word, wordId);
-					}
-					// store relationship: labelId->word->wordId
-					context.getVectorMetadata().putWordWithId(labelId, word, wordId);
-				}
-			}
 		}
-		
+		// generate word id
+		 Iterator<Entry<String,Term>> iter = 
+					context.getVectorMetadata().chiMergedTermVectorIterator();
+		while(iter.hasNext()) {
+			Entry<String,Term> entry = iter.next();
+			++wordNumber;
+			entry.getValue().setId(wordNumber);
+		}
 		// store metadata
 		context.getVectorMetadata().putLabelToIdPairs(globalLabelToIdMap);
 		context.getVectorMetadata().putIdToLabelPairs(globalIdToLabelMap);
 		
 		// output term vectors
-		outputTermVectors();
+		outputChiTermVector();
 	}
 
-	private void outputTermVectors() {
-		Iterator<Entry<Integer, Map<String, Integer>>> iter = context.getVectorMetadata().termVectorsMapIterator();
-		while(iter.hasNext()) {
-			Entry<Integer, Map<String, Integer>> entry = iter.next();
-			int labelId = entry.getKey();
-			String label = context.getVectorMetadata().getlabel(labelId);
-			BufferedWriter w = null;
-			try {
-				File file = getTermVectorFileName(labelId);
-				LOG.info("Term vector file: labelId=" + labelId + ", label=" + label + ", file=" + file);
-				w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), charSet));
-				Iterator<Entry<String, Integer>> wordIter = entry.getValue().entrySet().iterator();
-				while(wordIter.hasNext()) {
-					Entry<String, Integer> wordEntry = wordIter.next();
-					String word = wordEntry.getKey();
-					Integer wordId = wordEntry.getValue();
-					StringBuffer buf = new StringBuffer();
-					buf
-						.append(labelId).append("\t")
-						.append(word).append("\t")
-						.append(wordId);
-					LOG.debug("Write term vector: label=" + label + 
-							",datum=" + buf.toString());
-					w.write(buf.toString());
-					w.newLine();
-				}
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				if(w != null) {
-					try {
-						w.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+	private void outputChiTermVector() {
+		BufferedWriter w = null;
+		Iterator<Entry<String,Term>> iter = 
+				context.getVectorMetadata().chiMergedTermVectorIterator();
+		try {
+			w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
+					context.getFDMetadata().getChiTermVectorFile()), charSet));
+			while(iter.hasNext()) {
+				Entry<String,Term> entry = iter.next();
+				String word = entry.getKey();
+				Integer wordId = entry.getValue().getId();
+				StringBuffer buf = new StringBuffer();
+				buf
+					.append(word).append("\t")
+					.append(wordId);
+				LOG.debug("Write CHI term vector: word=" + word + 
+						", datum=" + buf.toString());
+				w.write(buf.toString());
+				w.newLine();
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(w != null) {
+				try {
+					w.close();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
 		}
-		
-	}
-
-	private File getTermVectorFileName(int labelId) {
-		String label = context.getVectorMetadata().getlabel(labelId);
-		String fileName = label + "-" + labelId + "-terms.txt";
-		File termVectorFile = new File(outputDir, fileName);
-		return termVectorFile;
 	}
 
 }
